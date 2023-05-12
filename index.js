e6c36ae7bb5a5aae14bf644b7a5de5ab6f332cbd
João Pablo <114818008+opabloliver@users.noreply.github.com>
2023-05-12 04:55:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const { Client, Intents, GatewayIntentBits, ActivityType, PermissionFlagsBits } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require("discord.js")
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const config = require("./config.json");
+
+client.login(config.token);
+
+
+
+module.exports = client;
+client.commands = new Discord.Collection();
+client.slashCommands = new Discord.Collection();
+client.config = require("./config.json");
+require("./Handler")(client);
+const { glob } = require("glob");
+const { promisify } = require("util");
+
+const globPromise = promisify(glob);
+
+client.once('ready', async () => {
+    console.log(`✅ - Logado em ${client.user.username} com sucesso! Estou em ${client.guilds.cache.size} servidores!`)
+    client.user.setPresence({
+        activities: [{ name: `15 comandos para ${client.guilds.cache.size} servidore!`, type: ActivityType.Playing }],
+        status: 'idle',
+      });
+})
+
+
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.guild) return;
+
+    if (interaction.isCommand()) {
+
+        const cmd = client.slashCommands.get(interaction.commandName);
+
+        if (!cmd)
+            return;
+
+        const args = [];
+
+        for (let option of interaction.options.data) {
+
+            if (option.type === "SUB_COMMAND") {
+                if (option.name) args.push(option.name);
+                option.options?.forEach((x) => {
+                    if (x.value) args.push(x.value);
+                });
+            } else if (option.value) args.push(option.value);
+        }
+
+        cmd.run(client, interaction, args);
+    }
+
+    if (interaction.isContextMenuCommand()) {
+        await interaction.deferReply({ ephemeral: false });
+        const command = client.slashCommands.get(interaction.commandName);
+        if (command) command.run(client, interaction);
+
+    }
+});
